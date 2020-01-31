de6a7c8e477389d681f2ef5dc83c1284e7a19cb4
Mark Videon <>
2020-01-31 20:05:24 +1030

    Interpolator script complete Player Controller boundaries enforced

--- Assets/Scripts/Interpolator.cs
+++ Assets/Scripts/Interpolator.cs
@@ -1,14 +1,65 @@
 ﻿using System;
 using System.Timers;
+using UnityEngine;
 
+public delegate void Listener();
+public delegate T Calculation<T>(T initial, T final,float proportion);
 
 public class Interpolator<T>
 {
     private Timer timer;
-    private T value;
+    public T value { get;  private set; }
+    private Listener OnInterval;
+    private Listener OnComplete;
+    private Calculation<T> Interpolation;
+
+    private float accrued;
+    private float length;
+    private int intervals;
+
+    private T first;
+    private T last;
 
     private const int SECONDS_TO_MILLISECONDS = 1000;
 
-    Interpolator<T>() {
+    public Interpolator(T initial, T final,
+        int intervalCount, float duration,
+        Calculation<T> calculationCallback,
+        Listener intervalCallback = null,
+        Listener completeCallback = null) {
+
+        length = duration * SECONDS_TO_MILLISECONDS;
+
+        timer = new Timer(length/intervalCount);
+        timer.Elapsed += OnTick;
+
+        intervals = intervalCount;
+        accrued = 0f;
+
+        // Initialise values
+        first = initial;
+        last = final;
+
+        Interpolation = calculationCallback;
+        OnInterval = intervalCallback;
+        OnComplete = completeCallback;
+
+        timer.Start();
+    }
+
+    public void OnTick(System.Object o, ElapsedEventArgs args)
+    {
+        if (accrued >= length)
+        {
+            timer.Stop();
+            timer.Dispose();
+            OnComplete?.Invoke();
+        } else
+        {
+            value = Interpolation(first, last, accrued / length);
+            OnInterval?.Invoke();
+        }
+
+        accrued += (float) timer.Interval;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,16 +7,19 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Transform topLeftBoundary;
     [SerializeField] private Transform bottomRightBoundary;
 
+    private bool enforceBounds = true;
+
     private void Start()
     {
         if (topLeftBoundary == null || bottomRightBoundary == null)
         {
+            enforceBounds = false;
             Debug.Log("A boundary is null!");
         } else
         {
             Assert.IsTrue(topLeftBoundary.position.y > bottomRightBoundary.position.y);
             Assert.IsTrue(topLeftBoundary.position.x < bottomRightBoundary.position.x);
-        } 
+        }
     }
 
     // Update is called once per frame
@@ -25,25 +28,53 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow) ||
             Input.GetKey(KeyCode.A))
 		{
-            this.gameObject.transform.position += speedMultiplier * Vector3.left * Time.deltaTime;
-		}
+            OptEnforceBounds(speedMultiplier * Vector3.left * Time.deltaTime);
+        }
 
         if (Input.GetKey(KeyCode.RightArrow) ||
             Input.GetKey(KeyCode.D))
         {
-            this.gameObject.transform.position += speedMultiplier * Vector3.right * Time.deltaTime;
+            OptEnforceBounds(speedMultiplier * Vector3.right * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.UpArrow) ||
             Input.GetKey(KeyCode.W))
         {
-            this.gameObject.transform.position += speedMultiplier * Vector3.up * Time.deltaTime;
+            OptEnforceBounds(speedMultiplier * Vector3.up * Time.deltaTime);
         }
 
         if (Input.GetKey(KeyCode.DownArrow) ||
             Input.GetKey(KeyCode.S))
         {
-            this.gameObject.transform.position += speedMultiplier * Vector3.down * Time.deltaTime;
+            OptEnforceBounds(speedMultiplier * Vector3.down * Time.deltaTime);
+        }
+
+    }
+
+    void EnforceBounds(Vector3 result)
+    {
+        Vector3 proposal = this.transform.position + result;
+        if (proposal.x > bottomRightBoundary.position.x ||
+            proposal.y < bottomRightBoundary.position.y ||
+            proposal.x < topLeftBoundary.position.x ||
+            proposal.y > topLeftBoundary.position.y)
+        {
+            Debug.Log("Would have taken out of bounds");
+        } else
+        {
+            this.transform.position += result;
+        }
+    }
+
+    void OptEnforceBounds (Vector3 result)
+    {
+        if (enforceBounds)
+        {
+            EnforceBounds(result);
+        }
+        else
+        {
+            this.transform.position += result;
         }
     }
 }
