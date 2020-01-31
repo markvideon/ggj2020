1214f3ea43886e6f2be544f8bd76c28654531e16
Mark Videon <>
2020-01-31 18:55:31 +1030

    Player Controller and Camera Technician scripts. CameraScene.

---
+++ Assets/Scripts/CameraTechnician.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTechnician : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float distanceThreshold = 0.1f;
+    [SerializeField] private float moveRate = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(this.transform.position, target.transform.position) > distanceThreshold)
+        {
+            Vector3 differential = target.transform.position - this.transform.position;
+            var nextX = differential.x * moveRate * Time.deltaTime;
+            var nextY = differential.y * moveRate * Time.deltaTime;
+            this.transform.position += new Vector3(nextX, nextY, 0f);
+        }
+    }
+
+    public void SetTarget(Transform nextTarget) => target = nextTarget;
+    public void SetMoveRate(float nextRate) => moveRate = nextRate;
+    public void SetDistanceThreshold(float nextThreshold) => distanceThreshold = nextThreshold;
+    
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speedMultiplier;
+    [SerializeField] private Transform topLeftBoundary;
+    [SerializeField] private Transform bottomRightBoundary;
+
+    private void Start()
+    {
+        if (topLeftBoundary == null || bottomRightBoundary == null)
+        {
+            Debug.Log("A boundary is null!");
+        } else
+        {
+            Assert.IsTrue(topLeftBoundary.position.y > bottomRightBoundary.position.y);
+            Assert.IsTrue(topLeftBoundary.position.x < bottomRightBoundary.position.x);
+        } 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow) ||
+            Input.GetKey(KeyCode.A))
+		{
+            this.gameObject.transform.position += speedMultiplier * Vector3.left * Time.deltaTime;
+		}
+
+        if (Input.GetKey(KeyCode.RightArrow) ||
+            Input.GetKey(KeyCode.D))
+        {
+            this.gameObject.transform.position += speedMultiplier * Vector3.right * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow) ||
+            Input.GetKey(KeyCode.W))
+        {
+            this.gameObject.transform.position += speedMultiplier * Vector3.up * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow) ||
+            Input.GetKey(KeyCode.S))
+        {
+            this.gameObject.transform.position += speedMultiplier * Vector3.down * Time.deltaTime;
+        }
+    }
+}
