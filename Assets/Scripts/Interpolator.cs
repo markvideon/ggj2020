448c109b1947c109d23eaa1d927a434bfd429f82
Mark Videon <>
2020-01-31 19:43:40 +1030

    Interpolator function

---
+++ Assets/Scripts/Interpolator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Timers;
+
+
+public class Interpolator<T>
+{
+    private Timer timer;
+    private T value;
+
+    private const int SECONDS_TO_MILLISECONDS = 1000;
+
+    Interpolator<T>() {
+    }
+}
